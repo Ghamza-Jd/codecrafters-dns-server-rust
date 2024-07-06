dc3b8bd493735963c5b7084fb390b7b56b58d610
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-07-07 02:14:37 +0300

    test: added dns message simple test

--- src/dns_message/message_impl.rs
+++ src/dns_message/message_impl.rs
@@ -11,3 +11,66 @@ impl DnsMessage {
         bytes
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        dns_answer::answer::DnsAnswer,
+        dns_header::header::{
+            AuthoritativeAnswer, DnsHeader, OpCode, QueryResponse, RecursionAvailability,
+            RecursionDesire, ResponseCode, Truncated, Z,
+        },
+        dns_message::message::DnsMessage,
+        dns_question::question::DnsQuestion,
+        resrec::{QClass, QType},
+    };
+    use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_as_bytes() {
+        let header = DnsHeader {
+            packet_id: 1234,
+            qr: QueryResponse::Reply,
+            opcode: OpCode::IQuery,
+            aa: AuthoritativeAnswer::NonAuthoritative,
+            tc: Truncated::NotTruncated,
+            rd: RecursionDesire::NotDesired,
+            ra: RecursionAvailability::NotAvailable,
+            z: Z::Reserved,
+            rcode: ResponseCode::NoErrorCondition,
+            qdcount: 1,
+            ancount: 1,
+            nscount: 0,
+            arcount: 0,
+        };
+        let question = DnsQuestion {
+            qname: vec!["codecrafters".to_string(), "io".to_string()],
+            qtype: QType::A,
+            qclass: QClass::IN,
+        };
+        let answer = DnsAnswer {
+            name: vec!["codecrafters".to_string(), "io".to_string()],
+            typ: QType::A,
+            class: QClass::IN,
+            ttl: 60,
+            rdlength: 4,
+            rddata: vec![Ipv4Addr::new(8, 8, 8, 8)],
+        };
+
+        let message = DnsMessage {
+            header,
+            questions: vec![question],
+            answer,
+        };
+        let bytes = message.as_bytes();
+
+        assert_eq!(
+            bytes,
+            [
+                4, 210, 136, 0, 0, 1, 0, 1, 0, 0, 0, 0, 12, 99, 111, 100, 101, 99, 114, 97, 102,
+                116, 101, 114, 115, 2, 105, 111, 0, 0, 1, 0, 1, 12, 99, 111, 100, 101, 99, 114, 97,
+                102, 116, 101, 114, 115, 2, 105, 111, 0, 0, 1, 0, 1, 0, 0, 0, 60, 0, 4, 8, 8, 8, 8
+            ]
+        );
+    }
+}
